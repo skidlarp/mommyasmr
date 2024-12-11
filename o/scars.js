dfcf1c681d61745dd91bca9427187a66bba38ae9
~$cars <165976429+skidlarp@users.noreply.github.com>
2024-12-11 00:13:32 -0500

    Update scars.js

--- o/scars.js
+++ o/scars.js
@@ -7,45 +7,30 @@ const r={
 };
 function D(...o){return window.enmity.modules.bulk(...o)}
 function c(...o){return window.enmity.modules.getByProps(...o)}
+const M=window.enmity.modules.common,
+      O=window.enmity.modules.common.Constants,
+      f=window.enmity.modules.common.Clipboard;
 const t=window.enmity.modules.common.React;
-const T=window.enmity.modules.common.StyleSheet;
-const l=window.enmity.modules.common.ColorMap;
-const settings = {
-    platform: "desktop",
-};
+const T=window.enmity.modules.common.StyleSheet,
+      l=window.enmity.modules.common.ColorMap;
+const v=window.enmity.modules.common.Toasts;
 
-function getPlatformSettings() {
-    switch (settings.platform) {
-        case "desktop":
-            return { browser: "Discord Client" };
-        case "web":
-            return { browser: "Chrome" };
-        case "mobile":
-            return { browser: "Discord iOS" };
-        case "embedded":
-            return { browser: "Discord Embedded" };
-        default:
-            return { browser: "Unknown" };
-    }
-}
-
-var PlatformSpoofer = {
+const PlatformSpoofer = {
     name: "PlatformSpoofer",
     version: "1.0.0",
-    description: "Spoof your platform or device",
+    description: "Spoof as Console platform",
     authors: [{ name: "$Bandwidth", id: "123456789012345678" }],
     onStart() {
         const patcher = window.enmity.patcher.create("PlatformSpoofer");
         patcher.before("_doIdentify", window, ([arg]) => {
             if (arg && arg.properties) {
-                Object.assign(arg.properties, getPlatformSettings());
+                arg.properties.browser = "Console";
             }
         });
     },
     onStop() {
         window.enmity.patcher.unpatchAll("PlatformSpoofer");
     },
-    settings,
 };
 
 V(PlatformSpoofer);
